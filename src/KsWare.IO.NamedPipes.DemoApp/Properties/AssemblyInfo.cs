ee6f7844867ec526f9761a37acbb04779b227bf7
SchreinerK <ks@ksware.de>
2021-08-20 00:11:12 +0200

    convert to sdk format. TargetFrameworks: net45;netcoreapp3.1;net5.0

--- src/KsWare.IO.NamedPipes.DemoApp/Properties/AssemblyInfo.cs
+++ src/KsWare.IO.NamedPipes.DemoApp/Properties/AssemblyInfo.cs
@@ -2,22 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("KsWare named pipes demo")]
-[assembly: AssemblyDescription("A demo app for named pipes.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KsWare")]
-[assembly: AssemblyProduct("KsWare.IO.NamedPipes")]
-[assembly: AssemblyCopyright("Copyright © 2018 by KsWare. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: Guid("2dd44b0e-c395-4198-a1f3-149e92240e74")]
-
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2")]
-[assembly: AssemblyInformationalVersion("0.1.2")]
 
--- src/KsWare.IO.NamedPipes/Properties/AssemblyInfo.cs
+++ src/KsWare.IO.NamedPipes/Properties/AssemblyInfo.cs
@@ -2,21 +2,8 @@
 using System.Runtime.InteropServices;
 using KsWare.IO.NamedPipes;
 
-[assembly: AssemblyTitle("KsWare.IO.NamedPipes")]
-[assembly: AssemblyDescription("A named pipes library.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KsWare")]
-[assembly: AssemblyProduct("KsWare.IO.NamedPipes")]
-[assembly: AssemblyCopyright("Copyright © 2018 by KsWare. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: Guid("378cac5b-1f20-4278-a129-9a9ce5243c7c")]
-
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2")]
-[assembly: AssemblyInformationalVersion("0.1.2")]
 
 // ReSharper disable once CheckNamespace
 namespace KsWare.IO.NamedPipes {
